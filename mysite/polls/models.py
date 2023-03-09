e3294bc15239e367be4566afb45046873c2a573b
Alan D <31675641+alanmicah@users.noreply.github.com>
2023-03-09 17:45:23 +0000

    interactive api

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -1,3 +1,28 @@
+import datetime
+
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+
+# Each Choice is associated with a Question
+class Question(models.Model):
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+
+    def __str__(self):
+        return self.question_text
+    
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+
+# It’s important to add __str__() methods to your models,
+# not only for your own convenience when dealing with the interactive prompt,
+# but also because objects’ representations are used throughout Django’s automatically-generated admin.
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.choice_text
