00bdbc4606bdfbac72481a7c93578b89ff2f9410
alanmicah <31675641+alanmicah@users.noreply.github.com>
2023-03-10 15:22:26 +0000

    adding views

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -2,6 +2,13 @@ from django.urls import path
 
 from . import views
 
+# Adding path() calls for the new added views
+# add namespaces to your URLconf for multiple apps
+# so django knows which app view to create a url when using the {% url %} template tag
+app_name = 'polls'
 urlpatterns = [
     path('', views.index, name='index'),
+    path('<int:question_id>/', views.detail, name='detail'),
+    path('<int:question_id>/results/', views.results, name='results'),
+    path('<int:question_id>/vote/', views.vote, name='vote'),
 ]
