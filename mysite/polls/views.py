6cf07bf32db96be833d2299d24f3e8b91faec8e3
alanmicah <31675641+alanmicah@users.noreply.github.com>
2023-03-22 12:14:06 +0000

    views, template, removing hardcoded URLS

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,6 +1,35 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse, Http404
+
+from .models import Question
+
 # Create your views here.
 
+# Load a template, fill a context and return an HttpResponse object
+# with the result of the rendered template.
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    context = {'latest_question_list': latest_question_list}
+    return render(request, 'polls/index.html', context)
+
+# Raises an Http404 exception if a question with the requested ID doesn't exist
+# def detail(request, question_id):
+#     try:
+#         question = Question.objects.get(pk=question_id)
+#     except Question.DoesNotExist:
+#         raise Http404("Question does not exist")
+#     return render(request, 'polls/detail.html', {'question': question})
+
+# Using ObjectDoesNotExist exception at higher level, or
+# having the model API raise Http404 instead would couple the model later to the view layer
+# Django's goal is to maintain loose coupling.
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'question': question})
+
+def results(request, question_id):
+    response = "You're looking at the results of question %s."
+    return HttpResponse(response % question_id)
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s." % question_id)
